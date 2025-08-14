77573c329a2d56ec3e62ec2e6b83df9ee8d8471a
Eja <ejajakwe@gmail.com>
2025-08-14 19:57:49 +0700

    test2

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://syncbe-production.up.railway.app'],
 
     'allowed_headers' => ['*'],
 
