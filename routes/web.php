dd1628607131f82e62254ce5242313b2f6d14112
Eja <ejajakwe@gmail.com>
2025-08-15 09:40:10 +0700

    test image 10

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/_once/storage-link', function () {
+    abort_unless(app()->environment('production'), 403);
+    \Illuminate\Support\Facades\Artisan::call('storage:link');
+    return 'storage:link done';
+});
